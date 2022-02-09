fb8db7b95a295b1c08a7f75e3700a94242932bfb
ivan <kulikovia.72@icloud.com>
2022-02-09 17:54:40 +0200

    fix global crashes

--- Sources/Global/safeCrash.swift
+++ Sources/Global/safeCrash.swift
@@ -1,14 +1,14 @@
-public func crash(
+public func safeCrash(
     _ message: String = "",
     _ file: String = #file,
     _ function: String = #function,
     _ line: Int = #line
-) -> Never {
+) {
     let filename = file.split(separator: "/").last.orEmpty
-    fatalError("Crash in \(filename): \(function) on line \(line)\(message.isEmpty ? "" : ". Message: \(message)")")
+    assertionFailure("Safe crash in \(filename): \(function) on line \(line)\(message.isEmpty ? "" : ". Message: \(message)")")
 }
 
-public func crashIf(
+public func safeCrashIf(
     _ condition: Bool,
     _ message: String = "",
     _ file: String = #file,
@@ -17,7 +17,7 @@ public func crashIf(
 ) {
     guard condition else { return }
 
-    crash(
+    safeCrash(
         message,
         file,
         function,
