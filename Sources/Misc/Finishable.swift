283308e16a32d62899eb889798e77c351e034fbd
ivan <kulikovia.72@icloud.com>
2022-02-18 06:09:23 +0200

    remove startable/finishable unused helpers

--- Sources/Misc/Finishable.swift
+++ Sources/Misc/Finishable.swift
@@ -1,31 +1,3 @@
 public protocol Finishable {
     func finish()
 }
-
-public protocol FinishableProvider: Finishable {
-    var finishable: Finishable { get }
-}
-
-extension Finishable where Self: FinishableProvider {
-    public func finish() {
-        finishable.finish()
-    }
-}
-
-public struct Finishables: Finishable {
-    public init(from finishables: [Finishable]) {
-        self.finishables = finishables
-    }
-
-    public init(from finishables: Finishable...) {
-        self.init(from: finishables)
-    }
-
-    private let finishables: [Finishable]
-
-    // MARK: - Finishable
-
-    public func finish() {
-        finishables.forEach { $0.finish() }
-    }
-}
--- Sources/Misc/Startable.swift
+++ Sources/Misc/Startable.swift
@@ -2,38 +2,8 @@ public protocol Startable {
     func start()
 }
 
-public protocol StartableProvider: Startable {
-    var startable: Startable { get }
-}
-
-extension Startable where Self: StartableProvider {
-    public func start() {
-        startable.start()
-    }
-}
-
-public struct Startables: Startable {
-    public init(from startables: [Startable]) {
-        self.startables = startables
-    }
-
-    public init(from startables: Startable...) {
-        self.init(from: startables)
-    }
-
-    private let startables: [Startable]
-
-    // MARK: - Startable
-
-    public func start() {
-        startables.forEach { $0.start() }
-    }
-}
-
-public struct Touch: Startable {
+public struct Awake: Startable {
     public init(_: Any?) { }
-
-    // MARK: - Startable
 
     public func start() { }
 }
