7965b6f3f690319dd7f2035c0bd0038f17b66c33
ivan <kulikovia.72@icloud.com>
2022-02-18 06:03:40 +0200

    add providers

---
+++ Sources/Misc/Providers.swift
@@ -0,0 +1,15 @@
+public protocol IntProvider {
+    var providee: Int { get }
+}
+
+public protocol StringProvider {
+    var providee: String { get }
+}
+
+public protocol DoubleProvider {
+    var providee: Double { get }
+}
+
+public protocol BoolProvider {
+    var providee: Bool { get }
+}
