f19cf37f0c36aa33db3ba98a9a9464098b259f7a
ivan <kulikovia.72@icloud.com>
2022-03-09 22:55:46 +0200

    add FileManager extensions

--- Sources/Extensions/FileManager+Extensions.swift
+++ Sources/Extensions/FileManager+Extensions.swift
@@ -15,11 +15,60 @@ extension FileManager {
         )
     }
 
-    public func cleanup(_ url: URL) throws {
-        for item in try contentsOfDirectory(
+    public func items(at url: URL) throws -> [URL] {
+        try contentsOfDirectory(
             at: url,
-            includingPropertiesForKeys: nil
-        ) {
+            includingPropertiesForKeys: nil,
+            options: .empty
+        )
+    }
+
+    public func files(at url: URL) throws -> [URL] {
+        try contentsOfDirectory(
+            at: url,
+            includingPropertiesForKeys: [.isRegularFileKey],
+            options: .empty
+        )
+    }
+
+    public func directories(at url: URL) throws -> [URL] {
+        try contentsOfDirectory(
+            at: url,
+            includingPropertiesForKeys: [.isDirectoryKey],
+            options: .empty
+        )
+    }
+
+    public func fileExists(at url: URL) -> Bool {
+        var isDirectory: ObjCBool = false
+
+        return fileExists(atPath: url.path, isDirectory: &isDirectory)
+    }
+
+    public func directoryExists(at url: URL) -> Bool {
+        var isDirectory: ObjCBool = true
+
+        return fileExists(atPath: url.path, isDirectory: &isDirectory)
+    }
+
+    public func isDirectoryEmpty(at url: URL) -> Bool {
+        (try? items(at: url))?.isEmpty == true
+    }
+
+    @discardableResult
+    public func createFile(at url: URL, contents: Data?) -> Bool {
+        createFile(
+            atPath: url.path,
+            contents: contents
+        )
+    }
+
+    public func createDirectory(at url: URL) throws {
+        try createDirectory(at: url, withIntermediateDirectories: true)
+    }
+
+    public func cleanup(_ directoryURL: URL) throws {
+        for item in try items(at: directoryURL) {
             try removeItem(at: item)
         }
     }
