4b567cfc7f8fae7a612abf6b5a2ce25a3b251b09
ivan <kulikovia.72@icloud.com>
2022-03-24 20:29:02 +0200

    add Sequence extensions

--- Sources/Extensions/Collection+Extensions.swift
+++ Sources/Extensions/Collection+Extensions.swift
@@ -8,18 +8,4 @@ extension Collection {
     public var isNotEmpty: Bool {
         !isEmpty
     }
-
-    public func unwrapped<T>() -> [T] where Element == Optional<T> {
-        compactMap { $0 }
-    }
-
-    public func asyncMap<T>(_ transform: (Element) async throws -> T) async rethrows -> [T] {
-        var result = [T]()
-
-        for element in self {
-            try await result.append(transform(element))
-        }
-
-        return result
-    }
 }
