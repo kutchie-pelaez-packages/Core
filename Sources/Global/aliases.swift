74fe1bab16010b4fe136bd125045c0f498287127
ivan <kulikovia.72@icloud.com>
2022-04-27 04:44:19 +0300

    remove Combine aliases

--- Sources/Global/aliases.swift
+++ Sources/Global/aliases.swift
@@ -1,11 +1,3 @@
-import Combine
-
 public typealias Action = () -> Void
 public typealias ActionWith<Arg> = (Arg) -> Void
 public typealias Resolver<Resolvee> = () -> Resolvee
-
-public typealias ValuePublisher<Value> = AnyPublisher<Value, Never>
-public typealias VoidPublisher = AnyPublisher<Void, Never>
-
-public typealias ValuePassthroughSubject<Value> = PassthroughSubject<Value, Never>
-public typealias VoidPassthroughSubject = PassthroughSubject<Void, Never>
